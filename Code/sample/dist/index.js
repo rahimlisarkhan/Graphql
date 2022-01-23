86dd6c720229896cb76652a8da7f3fb79d8dcca2
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-01-23 22:05:57 +0400

    update

---
+++ Code/sample/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function Textlog(text) {
+    return "Hellossss" + text;
+}
+console.log(Textlog("Sarkhan"));
---
+++ Code/sample/src/index.ts
@@ -0,0 +1,6 @@
+
+function Textlog(text: string): string {
+    return "Hellossss" + text
+}
+
+console.log(Textlog("Sarkhan"));
