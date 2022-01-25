c9eb82a8286cee3cdec0ef3b7a09a69948e11b4e
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-01-25 21:46:23 +0400

    update

--- Code/sample/dist/index.js
+++ Code/sample/dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiply_1 = require("./multiply");
 function Textlog(text) {
     return "Hellossss" + text;
 }
+console.log((0, multiply_1.multiply)(4, 4));
 console.log(Textlog("Sarkhan"));
--- Code/sample/src/index.ts
+++ Code/sample/src/index.ts
@@ -1,6 +1,10 @@
+import { multiply} from "./multiply"
 
 function Textlog(text: string): string {
     return "Hellossss" + text
 }
+
+
+console.log(multiply(4, 4));
 
 console.log(Textlog("Sarkhan"));
